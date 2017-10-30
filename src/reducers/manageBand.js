d2094cf70a09e191c5f59c8ce7a8db39dd0180de
Jennifer Volpe <jennifervolpe@Jennifers-MacBook-Pro.local>
2017-10-30 14:07:28 -0400

    Done.

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,9 +1,16 @@
+let id = 0
+
 export default function manageBand(state = {
   bands: []
 }, action) {
   switch (action.type) {
     case 'ADD_BAND':
+      id++
+      const band = Object.assign({}, action.band, { id: id });
       return { bands: state.bands.concat(action.band) }
+      case 'DELETE_TODO':
+      const bands = state.bands.filter(band => band.id !== action.id)
+      return { bands }
     default:
       return state;
   }
